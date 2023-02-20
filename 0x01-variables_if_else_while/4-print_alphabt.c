d128166ebd6accc6963d7e372e4c58cec4ddabf3
Cliffen Ojango <cliff.ojango@gmail.com>
2023-02-20 13:28:27 -0800

    Alpbets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets in lowercase
+ *
+ * except e and q
+ *
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
