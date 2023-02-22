c3fe20a31d75330cc9be99664685bb3c81b8418d
Cliffen Ojango <cliff.ojango@gmail.com>
2023-02-21 18:29:38 -0800

    Update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,4 @@
-#ifndef HEADER_H
-#define HEADER_H
-
-int main(void);
+int _putchar(char c);
 
 void print_alphabet(void);
 
@@ -13,5 +10,16 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
-#endif
+int _abs(int);
 
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
